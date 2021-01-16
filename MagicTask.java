89192e8dd2c61f36a1077f10058a1f94dd9e60f2
Maya Harris <72241568+mjoyharris@users.noreply.github.com>
2021-01-16 14:49:02 -0500

    Update MagicTask.java

--- MagicTask.java
+++ MagicTask.java
@@ -4,13 +4,19 @@ public class MagicTask {
 
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Welcome! How many tasks do you have today?");
+        
+        int numTask = scanner.nextInt();
+        
         // need to create a main menu! (this should come first)
         
         //figure out how to put image in java file...
         
         // input how many tasks do you have today? 
 
-        Scanner scanner = new Scanner(System.in); // them entering the task number... 
+        // them entering the task number... 
         
         // What are these tasks?
         
