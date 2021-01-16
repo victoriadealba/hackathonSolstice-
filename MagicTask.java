26cfd441c1cecc0f53b3b126e4288010f156186a
victoriadealba <73964921+victoriadealba@users.noreply.github.com>
2021-01-16 14:40:41 -0500

    Update MagicTask.java

--- MagicTask.java
+++ MagicTask.java
@@ -3,10 +3,29 @@ public class MagicTask {
 
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Hello Maya"); 
+        // need to create a main menu! (this should come first)
+        
+        //figure out how to put image in java file...
+        
+        // input how many tasks do you have today? 
+
+        Scanner scanner = new Scanner(System.in); // them entering the task number... 
+        
+        // What are these tasks?
+        
+        // break every three tasks
+        
+        // input strings of the name of their tasks. make a string array of their tasks. 
+        
+        // a random number gen based off of number of tasks... crosses off the number after the task is accomplished...
+        
+       
+        // after thought menu... 
+        
+        
+        
+      
 
     }
     }
