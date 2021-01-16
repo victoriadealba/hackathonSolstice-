682e10b19e1c6e189ecc178182ff60effa8d9c48
victoriadealba <73964921+victoriadealba@users.noreply.github.com>
2021-01-16 14:27:43 -0500

    Update MagicTask.java

--- MagicTask.java
+++ MagicTask.java
@@ -5,6 +5,8 @@ public class MagicTask {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Hello Maya"); 
 
     }
     }
